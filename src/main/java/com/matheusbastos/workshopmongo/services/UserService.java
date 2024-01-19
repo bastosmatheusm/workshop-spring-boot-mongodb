a0d6deb6000ccbbc39ce6e333315741f6ada5ed4
bastosmatheusm <matheusm.bastos@hotmail.com>
2024-01-18 23:15:12 -0300

    Insercao de usuario com POST

--- src/main/java/com/matheusbastos/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusbastos/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusbastos.workshopmongo.domain.User;
+import com.matheusbastos.workshopmongo.dto.UserDTO;
 import com.matheusbastos.workshopmongo.repository.UserRepository;
 import com.matheusbastos.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
